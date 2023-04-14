23dd525ce32ed267bdba58afd88c766fc22db4b1
Diji Durojaiye <akindijijaiyejaiye@gmail.com>
2023-04-14 01:52:01 +0100

    add function teaser

--- acedu-assignments/journal/src/journal.js
+++ acedu-assignments/journal/src/journal.js
@@ -14,3 +14,23 @@ export function Journal(title, body) {
     let vowelArr = ["a", "i", "o", "e", "u"];
     let totalVowels = 0;
     let totalConsonants = 0;
+    for (let i = 0; i < this.body.length; i++) {
+        if (vowelArr.includes(this.body[i])) {
+          totalVowels++;
+        }
+        else if (this.body[i] != " ") {
+          totalConsonants++;
+        }
+      }
+      return [totalVowels, totalConsonants];
+    };
+
+    export  function getTeaser(passage){
+        const wordSplit = passage.split(" ");
+        const teaserWord = [];
+        for(let i = 0; i < 9; i++){
+          teaserWord.push(wordSplit[i]);
+        }
+        return teaserWord;
+      }
+    
