5d1ce117d516571a46246076c9ba48ca74cbd967
Diji Durojaiye <akindijijaiyejaiye@gmail.com>
2023-04-14 01:49:46 +0100

    add prototype wordcounter

--- acedu-assignments/journal/src/journal.js
+++ acedu-assignments/journal/src/journal.js
@@ -2,3 +2,10 @@ export function Journal(title, body) {
     this.title = title;
     this.body = body;
   }
+
+  Journal.prototype.wordCounter = function () {
+    let totalWords = 0;
+    let wordArray = this.body.split(" ");
+    totalWords = wordArray.length;
+    return totalWords;
+  };
